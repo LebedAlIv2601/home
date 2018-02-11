01e9f1a2b8d1bf619c3d77181240f22f77e64b24
LebedAlIv2601 <32431414+LebedAlIv2601@users.noreply.github.com>
2018-02-11 18:35:49 +0300

    Add files via upload

---
+++ register/index.php
@@ -0,0 +1,39 @@
+<?php 
+include('db_connect.php');
+if(isset($_POST['login'])){
+	$login=$_POST['login'];
+	$password=$_POST['pass'];
+	$rePassword=$_POST['repass'];
+	$email=$_POST['email'];
+	if($pass===$repass){
+			$sql = "INSERT INTO users VALUES ('','$login','$password','$email')";
+	}else{
+		echo "Пароль не совпадает";
+}
+} 
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Регистрация</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div class="center">
+	<h1>Регистрация</h1>
+	<form action="index.php" method="post">
+	<input type="text" name="login" placeholder="Логин" class="pole"><br> 
+	<input type="password" name="pass" placeholder="Пароль" class="pole"><br> 
+	<input type="password" name="repass" placeholder="Повторите пароль" class="pole"><br> 
+	<input type="email" name="email" placeholder="email" class="pole"><br> 
+	<input type="submit" value="РЕгистрация" class="btn btn-success"> <br>
+	<input type="submit" value="ВОйти" class="btn btn-primary">
+	<a href="auth.php" type="submit" value="ВОйти" class="btn btn-primary"></a>
+		</div>
+</body>
+</html>
